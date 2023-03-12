a68e0ec4889e9f0769b6abdb3889851482f360dd
Pratham Bhoir <pratham.bhoir@buyceps.com>
2023-03-12 08:50:57 +0530

    following ka posts and koi bhi user ka post

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('photo-post/', views.PhotographAPI.as_view(), name = 'photo-post'),
     path('photo-list/', views.PhotographListAPI.as_view(), name = 'photo-list'),
+    path('photo-list/<int:pk>/', views.PhotographListIdAPI.as_view(), name = 'photo-list-id'),
+    path('photo-following/', views.PhotographFollowingListAPI.as_view(), name = 'photo-following'),
 ]
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -5,6 +5,7 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework import status,permissions
 from accounts.models import *
+from engagement.models import *
 
 
 class PhotographAPI(GenericAPIView):
@@ -20,10 +21,34 @@ class PhotographAPI(GenericAPIView):
         return Response({"response":"Success"}, status=status.HTTP_201_CREATED)
 
 
+class PhotographListIdAPI(ListAPIView):
+    serializer_class = PhotographSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = User.objects.get(id=self.kwargs['pk'])
+        queryset = Photograph.objects.filter(user=user)
+        return queryset
+
+
 class PhotographListAPI(ListAPIView):
     serializer_class = PhotographSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = Photograph.objects.filter(user=self.request.user)
+        return queryset
+
+
+class PhotographFollowingListAPI(ListAPIView):
+    serializer_class = PhotographSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        list_of_users = []
+        following_queryset = Follow.objects.filter(follower=self.request.user)
+        for following in following_queryset:
+            list_of_users.append(following.main_user.id)
+        print(list_of_users)
+        queryset = Photograph.objects.filter(user__in=list_of_users)
         return queryset
